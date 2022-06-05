a7804921d725fb78c4b05ece1a200a91f61de7a7
giguerch <ce.giguere@gmail.com>
2022-06-05 18:20:56 -0400

    Checked code style on APROPOS file & added and html files for apropos message

--- APROPOS_UI.py
+++ APROPOS_UI.py
@@ -7,10 +7,13 @@
 # WARNING! All changes made in this file will be lost!
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QDialog
 
+
 class Ui_APROPOS(QDialog):
+    '''Class Apropos to display the about menu '''
+
     def __init__(self, Dialog):
         super(Ui_APROPOS, self).__init__(Dialog)
         self.setObjectName("Dialog")
@@ -18,7 +21,6 @@ class Ui_APROPOS(QDialog):
         self.textBrowser = QtWidgets.QTextBrowser(self)
         self.textBrowser.setGeometry(QtCore.QRect(0, 0, 350, 100))
         self.textBrowser.setObjectName("textBrowser")
-        
         self.retranslateUi(self)
         QtCore.QMetaObject.connectSlotsByName(self)
         self.exec_()
@@ -26,10 +28,6 @@ class Ui_APROPOS(QDialog):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("self", "À propos"))
-        self.textBrowser.setHtml(_translate("self", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:11pt;\">Logiciel pour la gestion de mes factures</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:11pt;\">Création: Charles-Édouard Giguère</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\'; font-size:11pt; color:#0000ff;\">Version: 1.1</span></p></body></html>"))
+        with open('APROPOS.html', 'r', encoding="utf-8") as AP:
+            message = AP.read()
+        self.textBrowser.setHtml(_translate("self", message))
