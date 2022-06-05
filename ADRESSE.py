8c0c9ff7bf21ac0d9c973a82034dfef5816b91bb
giguerch <ce.giguere@gmail.com>
2022-06-05 17:11:23 -0400

    Adresse style check

--- ADRESSE.py
+++ ADRESSE.py
@@ -1,67 +1,68 @@
-### -*-       Coding: utf-8         -*-
+# -*-       Coding: utf-8         -*-
 
 
-### Interface pour gérer les adresses.
+# Interface pour gérer les adresses.
 
-##############################
-## Base de données.         ##
-##                          ##
-##  - ADRESSE               ##
-##    + [ID]                ##
-##    + [NOM]               ##
-##    + [LIEU1]             ##
-##    + [LIEU2]             ##
-##    + [ADRESSE]           ##
-##    + [VILLE]             ##
-##    + [PROVINCE]          ##
-##    + [CP]                ##
-##                          ##
-##############################
+############################
+# Base de données.         #
+#                          #
+#  - ADRESSE               #
+#    + [ID]                #
+#    + [NOM]               #
+#    + [LIEU1]             #
+#    + [LIEU2]             #
+#    + [ADRESSE]           #
+#    + [VILLE]             #
+#    + [PROVINCE]          #
+#    + [CP]                #
+#                          #
+############################
 
 
 import sqlite3
 
+
 class ADRESSE:
     """Sert à creer une adresse individuelle"""
-    
+
     def __init__(self,
-                 ID = 0,      NOM = '',   LIEU1 = '',    LIEU2 = '',
-                 ADRESSE = '', VILLE = '', PROVINCE = '', CP = ''   ):
+                 ID=0, NOM='', LIEU1='', LIEU2='',
+                 ADRESSE='', VILLE='', PROVINCE='', CP=''):
         """
-           Initialise les attributs. Certains (ou tous les) attributs 
+           Initialise les attributs. Certains (ou tous les) attributs
            peuvent être nuls ('')
         """
-        self.ID       = ID
-        self.NOM      = NOM
-        self.LIEU1    = LIEU1
-        self.LIEU2    = LIEU2
-        self.ADRESSE  = ADRESSE
-        self.VILLE    = VILLE
+        self.ID = ID
+        self.NOM = NOM
+        self.LIEU1 = LIEU1
+        self.LIEU2 = LIEU2
+        self.ADRESSE = ADRESSE
+        self.VILLE = VILLE
         self.PROVINCE = PROVINCE
-        self.CP       = CP
+        self.CP = CP
 
     def from_id(self, ID):
-        if ID != None:
+        if ID is not None:
             conn = None
             try:
                 conn = sqlite3.connect("FACTURE.sqlt")
-            except Error as e:
+            except sqlite3.Error as e:
                 print(e)
             cur = conn.cursor()
-            cur.execute("SELECT * FROM ADRESSE WHERE ID = {}".format(ID))
+            cur.execute("SELECT * FROM ADRESSE WHERE ID={}".format(ID))
             rows = cur.fetchall()
-            self.ID       = ID
-            self.NOM      = rows[0][1]
-            self.LIEU1    = rows[0][2]
-            self.LIEU2    = rows[0][3]
-            self.ADRESSE  = rows[0][4]
-            self.VILLE    = rows[0][5]
+            self.ID = ID
+            self.NOM = rows[0][1]
+            self.LIEU1 = rows[0][2]
+            self.LIEU2 = rows[0][3]
+            self.ADRESSE = rows[0][4]
+            self.VILLE = rows[0][5]
             self.PROVINCE = rows[0][6]
-            self.CP       = rows[0][7]            
+            self.CP = rows[0][7]
             conn.close()
-        
-    def update(self, sqlupdate = False, **kwargs):
-        """You can update one or more of the 5 attributes of clients"""        
+
+    def update(self, sqlupdate=False, **kwargs):
+        """You can update one or more of the 5 attributes of clients"""
         for key, value in kwargs.items():
             if "ID" == key:
                 self.ID = value
@@ -83,30 +84,30 @@ class ADRESSE:
             conn = None
             try:
                 conn = sqlite3.connect("FACTURE.sqlt")
-            except Error as e:
+            except sqlite3.Error as e:
                 print(e)
             cur = conn.cursor()
-            sqlstr = ("UPDATE ADRESSE SET NOM = \'{}\'," +
-                      "LIEU1     = \'{}\'," +
-                      "LIEU2     = \'{}\'," +
-                      "ADRESSE   = \'{}\'," +
-                      "VILLE     = \'{}\'," +
-                      "PROVINCE  = \'{}\'," +
-                      "CP        = \'{}\'"
-                      "WHERE ID = {}")
-            sqlstr = sqlstr.format(self.NOM.replace("'","''"),
-                                   self.LIEU1.replace("'","''"),
-                                   self.LIEU2.replace("'","''"),
-                                   self.ADRESSE.replace("'","''"),
-                                   self.VILLE.replace("'","''"),
-                                   self.PROVINCE.replace("'","''"),
-                                   self.CP.replace("'","''"),
+            sqlstr = ("UPDATE ADRESSE SET NOM=\'{}\'," +
+                      "LIEU1    =\'{}\'," +
+                      "LIEU2    =\'{}\'," +
+                      "ADRESSE  =\'{}\'," +
+                      "VILLE    =\'{}\'," +
+                      "PROVINCE =\'{}\'," +
+                      "CP       =\'{}\'"
+                      "WHERE ID={}")
+            sqlstr = sqlstr.format(self.NOM.replace("'", "''"),
+                                   self.LIEU1.replace("'", "''"),
+                                   self.LIEU2.replace("'", "''"),
+                                   self.ADRESSE.replace("'", "''"),
+                                   self.VILLE.replace("'", "''"),
+                                   self.PROVINCE.replace("'", "''"),
+                                   self.CP.replace("'", "''"),
                                    self.ID)
             print(sqlstr)
             cur.execute(sqlstr)
             conn.commit()
             conn.close()
-        
+
     def print(self):
         """Affiche les valeurs du Client """
         print(("{}:\n\t{}\n\t{}\n\t{}" +
@@ -118,19 +119,17 @@ class ADRESSE:
                                                   self.VILLE,
                                                   self.PROVINCE,
                                                   self.CP))
-        
+
     def __lt__(self, other):
         """ Fonction pour ordonner la liste de client par ordre alphabétique"""
-        left  = str(self.NOM) 
+        left = str(self.NOM)
         right = str(other.NOM)
         return left < right
 
 
-
-
 class ADRESSE_LIST:
     """Liste d'adresse de la BD"""
-    
+
     def __init__(self):
         """Initialise la liste d'adresse en allant chercher dans la BD"""
         self.CL = list()
@@ -138,57 +137,57 @@ class ADRESSE_LIST:
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         cur.execute("SELECT * FROM ADRESSE")
         rows = cur.fetchall()
         for row in rows:
             row = list(row)
-            for i,r in enumerate(row, start = 0):
-                if r == None:
+            for i, r in enumerate(row, start=0):
+                if r is None:
                     row[i] = ''
             self.CL.append(ADRESSE(row[0], row[1], row[2], row[3],
                                    row[4], row[5], row[6], row[7]))
             self.CLF.append(True)
-        for i,j in enumerate(self.CLF):
-            self.CLF[i] = True            
+        for i, j in enumerate(self.CLF):
+            self.CLF[i] = True
         self.CL.sort()
         conn.close()
 
-    def add(self, ID, NOM = '', LIEU1 = '', LIEU2 = '', ADR = '',
-            VILLE = '', PROVINCE = '', CP = ''):
+    def add(self, ID, NOM='', LIEU1='', LIEU2='', ADR='',
+            VILLE='', PROVINCE='', CP=''):
         """
         Ajout d'un nouveau client. L'ID doit être spécifié.
         """
 
         self.CL.append(ADRESSE(ID, NOM, LIEU1, LIEU2,
                                ADR, VILLE, PROVINCE,
-                               CP = CP))
+                               CP=CP))
         self.CLF.append(True)
         for i, v in enumerate(self.CLF):
             self.CLF[i] = True
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         cur.execute(("INSERT INTO ADRESSE(ID, NOM, LIEU1, LIEU2," +
                      " ADRESSE, VILLE, PROVINCE, CP) VALUES " +
                      "({},\'{}\',\'{}\',\'{}\',\'{}\',\'{}\'," +
                      "\'{}\',\'{}\')").format(ID,
-                                              NOM.replace("'","''"),
-                                              LIEU1.replace("'","''"),
-                                              LIEU2.replace("'","''"),
-                                              ADR.replace("'","''"),
-                                              VILLE.replace("'","''"),
-                                              PROVINCE.replace("'","''"),
-                                              CP.replace("'","''")))
+                                              NOM.replace("'", "''"),
+                                              LIEU1.replace("'", "''"),
+                                              LIEU2.replace("'", "''"),
+                                              ADR.replace("'", "''"),
+                                              VILLE.replace("'", "''"),
+                                              PROVINCE.replace("'", "''"),
+                                              CP.replace("'", "''")))
         conn.commit()
         conn.close()
         self.CL.sort()
-    
+
     def update(self, ID, **kwargs):
         """Update d'un client. Un ID doit être spécifié pour que ça marche """
         for i in self.CL:
@@ -196,63 +195,62 @@ class ADRESSE_LIST:
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 i.update(**kwargs)
                 cur = conn.cursor()
-                sqlstr = ("UPDATE ADRESSE SET NOM  = \'{}\'," +
-                          "LIEU1      = \'{}\'," +
-                          "LIEU2      = \'{}\',"
-                          "ADRESSE    = \'{}\'," +
-                          "VILLE      = \'{}\'," +
-                          "PROVINCE   = \'{}\'," +
-                          "CP         = \'{}\' " +
-                          "WHERE ID   = {}")
-                sqlstr = sqlstr.format(i.NOM.replace("'","''"),
-                                       i.LIEU1.replace("'","''"),
-                                       i.LIEU2.replace("'","''"),
-                                       i.ADRESSE.replace("'","''"),
-                                       i.VILLE.replace("'","''"),
-                                       i.PROVINCE.replace("'","''"),
-                                       i.CP.replace("'","''"),
+                sqlstr = ("UPDATE ADRESSE SET NOM =\'{}\'," +
+                          "LIEU1 = \'{}\'," +
+                          "LIEU2     =\'{}\',"
+                          "ADRESSE   =\'{}\'," +
+                          "VILLE     =\'{}\'," +
+                          "PROVINCE  =\'{}\'," +
+                          "CP        =\'{}\' " +
+                          "WHERE ID  ={}")
+                sqlstr = sqlstr.format(i.NOM.replace("'", "''"),
+                                       i.LIEU1.replace("'", "''"),
+                                       i.LIEU2.replace("'", "''"),
+                                       i.ADRESSE.replace("'", "''"),
+                                       i.VILLE.replace("'", "''"),
+                                       i.PROVINCE.replace("'", "''"),
+                                       i.CP.replace("'", "''"),
                                        i.ID)
                 cur.execute(sqlstr)
                 conn.commit()
                 conn.close()
         self.CL.sort()
-    
+
     def delete(self, ID):
-        for i,v in enumerate(self.CL):            
+        for i, v in enumerate(self.CL):
             if v.ID == ID:
-                self.CL.pop(i)                
+                self.CL.pop(i)
                 self.CLF.pop(i)
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 cur = conn.cursor()
-                cur.execute("DELETE FROM ADRESSE WHERE ID = {}".format(ID))
+                cur.execute("DELETE FROM ADRESSE WHERE ID={}".format(ID))
                 conn.commit()
                 conn.close()
-    
+
     def print(self):
         """Affichage de la liste de client dans la console"""
         for i, r in enumerate(self.CL):
             if self.CLF[i]:
                 r.print()
-    
+
     def max_id(self):
         """ Trouver le numéro d'identité"""
-        max = 0 
+        max = 0
         for i in self.CL:
             if max < i.ID:
                 max = i.ID
         return(max)
-                       
+
     def filter(self, filter):
         """ Filter les participants """
-        max = 0
         for i, row in enumerate(self.CL):
             if row.NOM.lower().find(filter.lower()) == -1:
                 self.CLF[i] = False
@@ -260,21 +258,20 @@ class ADRESSE_LIST:
                 self.CLF[i] = True
 
 
-    
-
 if __name__ == "__main__":
     AL1 = ADRESSE_LIST()
-    #AL1.print()
-    #AL1.delete(3)
-    #AL1.add((AL1.max_id() + 1), "Département d'ophtalmologie",
+    # AL1.print()
+    # AL1.delete(3)
+    # AL1.add((AL1.max_id() + 1), "Département d'ophtalmologie",
     #        "Faculté de Médecine", "Université de Montréal",
     #        "C.P. 6128, Succ. Centre-Ville", "Montréal",
     #        "Québec", "H3C3J7")
-    #AL1.print()
-    #AL1.update(2, ADRESSE = "5415, boul. l'Assomption", VILLE = "Montréal")
-    #AL1.update(1, LIEU1 = "Centre de recherche de l'institut universitaire en santé mentale de Montréal", LIEU2 = "Pavillon Fernand-Séguin", ADRESSE = "7331, rue Hochelaga", VILLE = "Montréal")
-    #AL1.filter("OPHT")
+    # AL1.print()
+    # AL1.update(2, ADRESSE="5415, boul. l'Assomption", VILLE="Montréal")
+    AL1.update(1, LIEU1="Centre de recherche de l'institut universitaire" +
+               "en santé mentale de Montréal",
+               LIEU2="Pavillon Fernand-Séguin",
+               ADRESSE="7331, rue Hochelaga", VILLE="Montréal")
+    # AL1.filter("OPHT")
     AL1.filter("Mai")
     AL1.print()
-
-    
--- ADRESSE_UI.py
+++ ADRESSE_UI.py
@@ -9,10 +9,13 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDialog
-from ADRESSE import ADRESSE, ADRESSE_LIST
+from ADRESSE import ADRESSE_LIST
 from functools import partial
 
+
 class Ui_ADRESSE(QDialog):
+    ''''Interface d'utilisateur des adresses '''
+
     def __init__(self, Dialog):
         super(Ui_ADRESSE, self).__init__()
         self.setObjectName("ADRESSE")
@@ -20,7 +23,8 @@ class Ui_ADRESSE(QDialog):
         self.buttonBox = QtWidgets.QDialogButtonBox(self)
         self.buttonBox.setGeometry(QtCore.QRect(310, 730, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel |
+                                          QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.Liste_Adresse = QtWidgets.QTreeWidget(self)
         self.Liste_Adresse.setGeometry(QtCore.QRect(60, 80, 501, 321))
@@ -137,7 +141,6 @@ class Ui_ADRESSE(QDialog):
         self.Ajouter_Adresse.clicked.connect(self.ajouter)
         self.Retirer_Adresse.clicked.connect(self.retirer)
         self.Chercher_Address.textChanged.connect(self.find)
-        
         QtCore.QMetaObject.connectSlotsByName(self)
         self.exec_()
 
@@ -159,46 +162,45 @@ class Ui_ADRESSE(QDialog):
             ID = self.AL.CL[ind].ID
             diag.id = ID
         else:
-            diag.id = -1     
-        
+            diag.id = -1
+
     def find(self):
-        txt_filter = self.Chercher_Address.text()        
+        txt_filter = self.Chercher_Address.text()
         if txt_filter != "Chercher Client":
-             self.AL.filter(txt_filter)
+            self.AL.filter(txt_filter)
         self.reload()
-        
-        
-        
+
     def reload(self):
         self.Liste_Adresse.clear()
         test = -1
-        for i,j in enumerate(self.AL.CL):
+        for i, j in enumerate(self.AL.CL):
             if self.AL.CLF[i]:
                 test = test + 1
                 item_0 = QtWidgets.QTreeWidgetItem(self.Liste_Adresse)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                item_1 = QtWidgets.QTreeWidgetItem(item_0)
-                self.Liste_Adresse.topLevelItem(test).setText(0, j.NOM)
-                self.Liste_Adresse.topLevelItem(test).child(0).setText(0, j.LIEU1)
-                self.Liste_Adresse.topLevelItem(test).child(1).setText(0, j.LIEU2)
-                self.Liste_Adresse.topLevelItem(test).child(2).setText(0, j.ADRESSE)
-                self.Liste_Adresse.topLevelItem(test).child(3).setText(0, j.VILLE)
-                self.Liste_Adresse.topLevelItem(test).child(4).setText(0, j.PROVINCE)
-                self.Liste_Adresse.topLevelItem(test).child(5).setText(0, j.CP)
-                
+                QtWidgets.QTreeWidgetItem(item_0)
+                QtWidgets.QTreeWidgetItem(item_0)
+                QtWidgets.QTreeWidgetItem(item_0)
+                QtWidgets.QTreeWidgetItem(item_0)
+                QtWidgets.QTreeWidgetItem(item_0)
+                QtWidgets.QTreeWidgetItem(item_0)
+                top = self.Liste_Adresse.topLevelItem(test)
+                top.setText(0, j.NOM)
+                top.child(0).setText(0, j.LIEU1)
+                top.child(1).setText(0, j.LIEU2)
+                top.child(2).setText(0, j.ADRESSE)
+                top.child(3).setText(0, j.VILLE)
+                top.child(4).setText(0, j.PROVINCE)
+                top.child(5).setText(0, j.CP)
+
     def ajouter(self):
-        self.AL.add(ID       = self.AL.max_id() + 1,
-                    NOM      = self.Nom_Adresse.text(),
-                    LIEU1    = self.Lieu1_Edit.text(),
-                    LIEU2    = self.Lieu2_Edit.text(),
-                    ADR      = self.Adresse_Edit.text(),
-                    VILLE    = self.Ville_Edit.text(),
-                    PROVINCE = self.Province_Combo.currentText(),
-                    CP       = self.CP_Edit.text())
+        self.AL.add(ID=self.AL.max_id() + 1,
+                    NOM=self.Nom_Adresse.text(),
+                    LIEU1=self.Lieu1_Edit.text(),
+                    LIEU2=self.Lieu2_Edit.text(),
+                    ADR=self.Adresse_Edit.text(),
+                    VILLE=self.Ville_Edit.text(),
+                    PROVINCE=self.Province_Combo.currentText(),
+                    CP=self.CP_Edit.text())
         self.reload()
 
     def retirer(self):
@@ -214,13 +216,16 @@ class Ui_ADRESSE(QDialog):
                 ind = i
                 break
         ID = self.AL.CL[ind].ID
-        self.AL.delete(ID)        
+        self.AL.delete(ID)
         self.reload()
-        
+
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.Liste_Adresse.headerItem().setText(0, _translate("Dialog", "Adresse de Facturation"))
+        self.Liste_Adresse.headerItem().setText(0,
+                                                _translate("Dialog",
+                                                           "Adresse de " +
+                                                           "Facturation"))
         __sortingEnabled = self.Liste_Adresse.isSortingEnabled()
         self.Liste_Adresse.setSortingEnabled(False)
         self.Liste_Adresse.setSortingEnabled(__sortingEnabled)
@@ -246,8 +251,9 @@ class Ui_ADRESSE(QDialog):
         self.label_10.setText(_translate("Dialog", "Code Postal"))
         self.label_5.setText(_translate("Dialog", "Lieu 1"))
         self.label_8.setText(_translate("Dialog", "Ville"))
-        self.Ajouter_Adresse.setText(_translate("Dialog", "Ajouter à la liste"))
-        self.Retirer_Adresse.setText(_translate("Dialog", "Retirer à la liste"))
+        self.Ajouter_Adresse.setText(_translate("Dialog",
+                                                "Ajouter à la liste"))
+        self.Retirer_Adresse.setText(_translate("Dialog",
+                                                "Retirer à la liste"))
         self.Province_Combo.setItemText(0, _translate("Dialog", "Québec"))
         self.Province_Combo.setItemText(1, _translate("Dialog", "Ontario"))
-
