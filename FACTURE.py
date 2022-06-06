921b89bb2332d60b796f43eb29aad050a9947bee
giguerch <ce.giguere@gmail.com>
2022-06-06 00:11:05 -0400

    Fichiers FACTURE checked syntax

--- FACTURE.py
+++ FACTURE.py
@@ -1,68 +1,70 @@
-### -*-  coding: utf-8 -*-
-### Auteur: Charles-Édouard Giguère 
+# -*-  coding: utf-8 -*-
+# Auteur: Charles-Édouard Giguère
 
-### Classe pour gérer un objet de type FACTURE. 
+# Classe pour gérer un objet de type FACTURE.
 
-##############################
-## Base de données.         ##
-##                          ##
-##  - FACTURE               ##
-##    + [ID]                ##
-##    + [PERIODE_MIN]       ##
-##    + [PERIODE_MAX]       ##
-##    + [DATE]              ##
-##    + [ID_CLIENT]         ##
-##    + [ID_STATUT]         ##
-##    + [ID_TARIF]          ##
-##                          ##
+#############################
+# Base de données.         ##
+#                          ##
+#  - FACTURE               ##
+#    + [ID]                ##
+#    + [PERIODE_MIN]       ##
+#    + [PERIODE_MAX]       ##
+#    + [DATE]              ##
+#    + [ID_CLIENT]         ##
+#    + [ID_STATUT]         ##
+#    + [ID_TARIF]          ##
+#                          ##
 ##############################
 
 import sqlite3
 
+
 class FACTURE:
     """ Sert à créer un client individuel"""
-    def __init__(self, ID = '', PROJET = '', PERIODE_MIN_MS = '',
-                 PERIODE_MIN_YR = '', PERIODE_MAX_MS = '', PERIODE_MAX_YR = '',
-                 DATE = '', ID_CLIENT = '', ID_STATUT = '', ID_TARIF = '',
-                 FACTURE_PDF = ''):
-        self.ID             = ID
-        self.PROJET         = PROJET
+
+    def __init__(self, ID='', PROJET='', PERIODE_MIN_MS='',
+                 PERIODE_MIN_YR='', PERIODE_MAX_MS='', PERIODE_MAX_YR='',
+                 DATE='', ID_CLIENT='', ID_STATUT='', ID_TARIF='',
+                 FACTURE_PDF=''):
+        self.ID = ID
+        self.PROJET = PROJET
         self.PERIODE_MIN_MS = PERIODE_MIN_MS
         self.PERIODE_MIN_YR = PERIODE_MIN_YR
         self.PERIODE_MAX_MS = PERIODE_MAX_MS
         self.PERIODE_MAX_YR = PERIODE_MAX_YR
-        self.DATE           = DATE
-        self.ID_CLIENT      = ID_CLIENT
-        self.ID_STATUT      = ID_STATUT
-        self.ID_TARIF       = ID_TARIF
-        self.FACTURE_PDF    = FACTURE_PDF
-        
+        self.DATE = DATE
+        self.ID_CLIENT = ID_CLIENT
+        self.ID_STATUT = ID_STATUT
+        self.ID_TARIF = ID_TARIF
+        self.FACTURE_PDF = FACTURE_PDF
 
     def from_id(self, ID):
-        if ID != None:
+        if ID is not None:
             conn = None
             try:
                 conn = sqlite3.connect("FACTURE.sqlt")
-            except Error as e:
+            except sqlite3.Error as e:
                 print(e)
             cur = conn.cursor()
             cur.execute("SELECT * FROM FACTURE WHERE ID = {}".format(ID))
             rows = cur.fetchall()
-            #print(rows)
-            self.ID             = ID
-            self.PROJET         = rows[0][1]
+            # print(rows)
+            self.ID = ID
+            self.PROJET = rows[0][1]
             self.PERIODE_MIN_MS = rows[0][2]
             self.PERIODE_MIN_YR = rows[0][3]
             self.PERIODE_MAX_MS = rows[0][4]
             self.PERIODE_MAX_YR = rows[0][5]
-            self.DATE           = rows[0][6]
-            self.ID_CLIENT      = rows[0][7]
-            self.ID_STATUT      = rows[0][8]
-            self.ID_TARIF       = rows[0][9]
-            self.FACTURE_PDF    = rows[0][10]
-            
-    def update(self, sqlupdate = False, **kwargs):
+            self.DATE = rows[0][6]
+            self.ID_CLIENT = rows[0][7]
+            self.ID_STATUT = rows[0][8]
+            self.ID_TARIF = rows[0][9]
+            self.FACTURE_PDF = rows[0][10]
+
+    def update(self, sqlupdate=False, **kwargs):
         """You can update one or more of the 9 attributes of FACTURE"""
+
         for key, Value in kwargs.items():
             if "ID" == key:
                 self.ID = Value
@@ -88,45 +90,45 @@ class FACTURE:
                 self.FACTURE_PDF = Value
 
     def print(self):
-        if self.PERIODE_MAX_MS != None:
+        if self.PERIODE_MAX_MS is not None:
             print(("#{}:\n\t{}\n\tPériode {}/{} - {}/{}" +
                    "\n\tDATE: {}\n\tCLIENT: {} STATUT: {} TARIF: {}" +
                    "\n\tPDF CRÉÉ: {}"
-            ).format(self.ID,
-                     self.PROJET,
-                     self.PERIODE_MIN_MS,
-                     self.PERIODE_MIN_YR,
-                     self.PERIODE_MAX_MS,
-                     self.PERIODE_MAX_YR,
-                     self.DATE,
-                     self.ID_CLIENT,
-                     self.ID_STATUT,
-                     self.ID_TARIF,
-                     self.FACTURE_PDF))
+                   ).format(self.ID,
+                            self.PROJET,
+                            self.PERIODE_MIN_MS,
+                            self.PERIODE_MIN_YR,
+                            self.PERIODE_MAX_MS,
+                            self.PERIODE_MAX_YR,
+                            self.DATE,
+                            self.ID_CLIENT,
+                            self.ID_STATUT,
+                            self.ID_TARIF,
+                            self.FACTURE_PDF))
         else:
             print(("#{}:\n\t{}\n\tPériode {}/{}" +
                    "\n\tDATE: {}\n\tCLIENT: {} STATUT: {} TARIF: {}" +
                    "\n\tPDF CRÉÉ: {}"
-            ).format(self.ID,
-                     self.PROJET,
-                     self.PERIODE_MIN_MS,
-                     self.PERIODE_MIN_YR,
-                     self.DATE,
-                     self.ID_CLIENT,
-                     self.ID_STATUT,
-                     self.ID_TARIF,
-                     self.FACTURE_PDF))
+                   ).format(self.ID,
+                            self.PROJET,
+                            self.PERIODE_MIN_MS,
+                            self.PERIODE_MIN_YR,
+                            self.DATE,
+                            self.ID_CLIENT,
+                            self.ID_STATUT,
+                            self.ID_TARIF,
+                            self.FACTURE_PDF))
 
     def __lt__(self, other):
         """ Fonction pour ordonner la liste de client par ordre alphabétique"""
-        left  = str(self.ID) 
+        left = str(self.ID)
         right = str(other.ID)
         return left < right
 
 
 class FACTURE_LIST:
     """Liste d'adresse de la BD"""
-    
+
     def __init__(self):
         """Initialise la liste d'adresse en allant chercher dans la BD"""
         self.FL = list()
@@ -134,29 +136,29 @@ class FACTURE_LIST:
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         cur.execute("SELECT * FROM FACTURE ORDER BY DATE DESC")
         rows = cur.fetchall()
         for row in rows:
             row = list(row)
-            for i,r in enumerate(row, start = 0):
-                if r == None:
+            for i, r in enumerate(row, start=0):
+                if r is None:
                     row[i] = ''
             self.FL.append(FACTURE(row[0], row[1], row[2], row[3],
                                    row[4], row[5], row[6], row[7],
                                    row[8], row[9], row[10]))
             self.FLF.append(True)
-        for i,j in enumerate(self.FLF):
-            self.FLF[i] = True            
-        self.FL.sort(reverse = True)
+        for i, j in enumerate(self.FLF):
+            self.FLF[i] = True
+        self.FL.sort(reverse=True)
         conn.close()
 
-    def add(self, ID, PROJET = '', PERIODE_MIN_MS = '',
-            PERIODE_MIN_YR = '', PERIODE_MAX_MS = '', PERIODE_MAX_YR = '',
-            DATE = '', ID_CLIENT = '', ID_STATUT = '', ID_TARIF = '',
-            FACTURE_PDF = ''):
+    def add(self, ID, PROJET='', PERIODE_MIN_MS='',
+            PERIODE_MIN_YR='', PERIODE_MAX_MS='', PERIODE_MAX_YR='',
+            DATE='', ID_CLIENT='', ID_STATUT='', ID_TARIF='',
+            FACTURE_PDF=''):
         """
         Ajout d'une nouvelle facture. L'ID doit être spécifié.
         """
@@ -170,7 +172,7 @@ class FACTURE_LIST:
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         if PERIODE_MAX_MS == '':
@@ -179,7 +181,8 @@ class FACTURE_LIST:
             PERIODE_MAX_YR = "Null"
         sqlstr = ("INSERT INTO FACTURE(ID, PROJET, PERIODE_MIN_MS, " +
                   "PERIODE_MIN_YR, PERIODE_MAX_MS, PERIODE_MAX_YR, " +
-                  "DATE, ID_CLIENT, ID_STATUT, ID_TARIF, FACTURE_PDF) VALUES " +
+                  "DATE, ID_CLIENT, ID_STATUT, ID_TARIF, FACTURE_PDF)" +
+                  " VALUES " +
                   " ({}, \'{}\', {}, {}, {}, {}," +
                   "\'{}\', {}, {}, {}, {})").format(ID,
                                                     PROJET.replace("'", "''"),
@@ -193,8 +196,8 @@ class FACTURE_LIST:
         cur.execute(sqlstr)
         conn.commit()
         conn.close()
-        self.FL.sort(reverse = True)
-    
+        self.FL.sort(reverse=True)
+
     def update(self, ID, **kwargs):
         """Update d'un client. Un ID doit être spécifié pour que ça marche """
         for i in self.FL:
@@ -202,22 +205,22 @@ class FACTURE_LIST:
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 i.update(**kwargs)
                 cur = conn.cursor()
                 sqlstr = ("UPDATE FACTURE SET PROJET  = \'{}\'," +
                           "PERIODE_MIN_MS = {}," +
-                          "PERIODE_MIN_YR = {},"
+                          "PERIODE_MIN_YR = {}," +
                           "PERIODE_MAX_MS = {}," +
                           "PERIODE_MAX_YR = {}," +
-                          "DATE           = \'{}\'," + 
+                          "DATE           = \'{}\'," +
                           "ID_CLIENT      = {}," +
-                          "ID_STATUT      = {},"+
-                          "ID_TARIF       = {},"+
+                          "ID_STATUT      = {}," +
+                          "ID_TARIF       = {}," +
                           "FACTURE_PDF    = {} " +
                           "WHERE ID   = {}")
-                sqlstr = sqlstr.format(i.PROJET.replace("'","''"),
+                sqlstr = sqlstr.format(i.PROJET.replace("'", "''"),
                                        i.PERIODE_MIN_MS,
                                        i.PERIODE_MIN_YR,
                                        i.PERIODE_MAX_MS,
@@ -230,40 +233,39 @@ class FACTURE_LIST:
                 cur.execute(sqlstr)
                 conn.commit()
                 conn.close()
-        self.FL.sort(reverse = True)
-    
+        self.FL.sort(reverse=True)
+
     def delete(self, ID):
-        for i,v in enumerate(self.FL):            
+        for i, v in enumerate(self.FL):
             if v.ID == ID:
-                self.FL.pop(i)                
+                self.FL.pop(i)
                 self.FLF.pop(i)
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 cur = conn.cursor()
                 cur.execute("DELETE FROM FACTURE WHERE ID = {}".format(ID))
                 conn.commit()
                 conn.close()
-    
+
     def print(self):
         """Affichage de la liste de client dans la console"""
         for i, r in enumerate(self.FL):
             if self.FLF[i]:
                 r.print()
-    
+
     def max_id(self):
         """ Trouver le numéro d'identité"""
-        max = 0 
+        max = 0
         for i in self.FL:
             if max < i.ID:
                 max = i.ID
         return(max)
-                       
+
     def filter(self, filter):
         """ Filter les participants """
-        max = 0
         for i, row in enumerate(self.FL):
             if row.PROJET.lower().find(filter.lower()) == -1:
                 self.FLF[i] = False
@@ -271,18 +273,16 @@ class FACTURE_LIST:
                 self.FLF[i] = True
 
 
-    
-            
 if __name__ == "__main__":
-    ### FAC1 = FACTURE()
-    ### FAC1.from_id(8739926)
-    ### FAC1.print()
+    # FAC1 = FACTURE()
+    # FAC1.from_id(8739926)
+    # FAC1.print()
     FL1 = FACTURE_LIST()
     FL1.print()
-    ### print(FL1.max_id())
-    ### FL1.filter("prof")
-    ### FL1.print()
-    ### FL1.filter("")
-    ### FL1.print()
-    ### FL1.add(8739928, 'Honoraire professionel',
-    ###         6, 2014, '', '', '2014-06-25', 6, 2, 1, 0)
+    # print(FL1.max_id())
+    # FL1.filter("prof")
+    # FL1.print()
+    # FL1.filter("")
+    # FL1.print()
+    # FL1.add(8739928, 'Honoraire professionel',
+    #         6, 2014, '', '', '2014-06-25', 6, 2, 1, 0)
--- FACTURE_ACTIVITE.py
+++ FACTURE_ACTIVITE.py
@@ -1,34 +1,35 @@
-### -*-  coding: utf-8 -*-
-### Auteur: Charles-Édouard Giguère 
+# -*-  coding: utf-8 -*-
+# Auteur: Charles-Édouard Giguère
 
-### Classe pour gérer un objet de type FACTURE. 
+# Classe pour gérer un objet de type FACTURE.
 
-##############################
-## Base de données.         ##
-##                          ##
-##  - FACTURE               ##
-##    + [ID]                ##
-##    + [ID_FACTURE]        ##
-##    + [DATE]              ##
-##    + [DESCRIPTION]       ##
-##    + [NB_HEURES]         ##
-##                          ##
-##############################
-
+############################
+# Base de données.         #
+#                          #
+#  - FACTURE               #
+#    + [ID]                #
+#    + [ID_FACTURE]        #
+#    + [DATE]              #
+#    + [DESCRIPTION]       #
+#    + [NB_HEURES]         #
+#                          #
+############################
 
 import sqlite3
 
+
 class FACTURE_ACTIVITE:
     """ Sert à créer un client individuel"""
-    def __init__(self, ID = '', ID_FACTURE = '', DATE = '',
-                 DESCRIPTION = '', NB_HEURES = ''):
-        self.ID             = ID
-        self.ID_FACTURE     = ID_FACTURE
-        self.DATE           = DATE
-        self.DESCRIPTION    = DESCRIPTION
-        self.NB_HEURES      = NB_HEURES        
-            
-    def update(self, sqlupdate = False, **kwargs):
+
+    def __init__(self, ID='', ID_FACTURE='', DATE='',
+                 DESCRIPTION='', NB_HEURES=''):
+        self.ID = ID
+        self.ID_FACTURE = ID_FACTURE
+        self.DATE = DATE
+        self.DESCRIPTION = DESCRIPTION
+        self.NB_HEURES = NB_HEURES
+
+    def update(self, sqlupdate=False, **kwargs):
         """You can update one or more of the 9 attributes of FACTURE"""
         for key, Value in kwargs.items():
             if "ID" == key:
@@ -41,89 +42,95 @@ class FACTURE_ACTIVITE:
                 self.DESCRIPTION = Value
             if "NB_HEURES" == key:
                 self.NB_HEURES = Value
-                
-    def print(self):       
-            print("{} {} {} {} {}".format(self.ID,
-                                          self.ID_FACTURE,
-                                          self.DATE,
-                                          self.DESCRIPTION,
-                                          self.NB_HEURES))
+
+    def print(self):
+        print("{} {} {} {} {}".format(self.ID,
+                                      self.ID_FACTURE,
+                                      self.DATE,
+                                      self.DESCRIPTION,
+                                      self.NB_HEURES))
 
     def __lt__(self, other):
         """ Fonction pour ordonner la liste de client par ordre alphabétique"""
-        left  = str(self.ID) 
+        left = str(self.ID)
         right = str(other.ID)
         return left < right
 
+
 class FACTURE_ACTIVITE_LIST:
     """Liste d'adresse de la BD"""
-    
+
     def __init__(self):
         """Initialise la liste d'adresse en allant chercher dans la BD"""
         self.FAL = list()
-        
+
     def by_id(self, ID_FACTURE):
         self.FAL.clear()
         self.ID_FACTURE = ID_FACTURE
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM FACTURE_ACTIVITE WHERE ID_FACTURE = {}".format(self.ID_FACTURE))
+        instruct = ("SELECT * FROM FACTURE_ACTIVITE " +
+                    "WHERE ID_FACTURE = {}").format(self.ID_FACTURE)
+        cur.execute(instruct)
         rows = cur.fetchall()
         if len(rows) > 0:
             for row in rows:
                 row = list(row)
-                for i,r in enumerate(row, start = 0):
-                    if r == None:
+                for i, r in enumerate(row, start=0):
+                    if r is None:
                         row[i] = ''
-                self.FAL.append(FACTURE_ACTIVITE(row[0], row[1], row[2], row[3], row[4]))   
+                self.FAL.append(FACTURE_ACTIVITE(row[0], row[1],
+                                                 row[2], row[3], row[4]))
         self.FAL.sort()
         conn.close()
-        
-    def add(self, ID, ID_FACTURE = '', DATE = '',
-            DESCRIPTION = '', NB_HEURES = ''):
+
+    def add(self, ID, ID_FACTURE='', DATE='',
+            DESCRIPTION='', NB_HEURES=''):
         """
         Ajout d'une nouvelle facture. L'ID doit être spécifié.
         """
+
         self.FAL.append(FACTURE_ACTIVITE(ID, ID_FACTURE, DATE,
                                          DESCRIPTION, NB_HEURES))
 
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         sqlstr = ("INSERT INTO FACTURE_ACTIVITE(ID, ID_FACTURE, DATE, " +
                   "DESCRIPTION, NB_HEURES) VALUES " +
-                  " ({}, {}, \'{}\', \'{}\', {})").format(ID,
-                                                          ID_FACTURE,
-                                                          DATE,
-                                                          DESCRIPTION.replace("'", "''"),
-                                                          NB_HEURES)
+                  " ({}, {}, \'{}\', \'{}\', {})"
+                  ).format(ID,
+                           ID_FACTURE,
+                           DATE,
+                           DESCRIPTION.replace("'", "''"),
+                           NB_HEURES)
         cur.execute(sqlstr)
         conn.commit()
         conn.close()
         self.FAL.sort()
 
     def update(self, ID, **kwargs):
-        """Update d'un client. Un ID doit être spécifié pour que ça marche """
+        """ Update d'un client. Un ID doit être spécifié pour que ça marche """
         for i in self.FAL:
             if i.ID == ID:
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 i.update(**kwargs)
                 cur = conn.cursor()
                 sqlstr = ("UPDATE FACTURE_ACTIVITE SET ID_FACTURE  = {}," +
                           "DATE = \'{}\'," +
-                          "DESCRIPTION = \'{}\',"
-                          "NB_HEURES = {} " 
+                          "DESCRIPTION = \'{}\'," +
+                          "NB_HEURES = {} " +
                           "WHERE ID   = {}")
                 sqlstr = sqlstr.format(i.ID_FACTURE,
                                        i.DATE,
@@ -136,41 +143,42 @@ class FACTURE_ACTIVITE_LIST:
         self.FAL.sort()
 
     def delete(self, ID):
-        for i,v in enumerate(self.FAL):            
+        for i, v in enumerate(self.FAL):
             if v.ID == ID:
-                self.FAL.pop(i)                
+                self.FAL.pop(i)
                 conn = None
                 try:
                     conn = sqlite3.connect("FACTURE.sqlt")
-                except Error as e:
+                except sqlite3.Error as e:
                     print(e)
                 cur = conn.cursor()
-                cur.execute("DELETE FROM FACTURE_ACTIVITE WHERE ID = {}".format(ID))
+                cur.execute(("DELETE FROM FACTURE_ACTIVITE"
+                             " WHERE ID = {}").format(ID))
                 conn.commit()
-                conn.close()    
+                conn.close()
+
     def print(self):
         """Affichage de la liste des activités de facture dans la console"""
         for i, r in enumerate(self.FAL):
-                r.print()
-    
+            r.print()
+
     def max_id(self):
         """ Trouver le numéro d'identité"""
         conn = None
         try:
             conn = sqlite3.connect("FACTURE.sqlt")
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
         cur = conn.cursor()
         cur.execute("SELECT MAX(ID) FROM FACTURE_ACTIVITE")
         rows = cur.fetchall()
         return(int(rows[0][0]))
-    
-            
-if __name__ == "__main__":  
 
+
+if __name__ == "__main__":
     FAL1 = FACTURE_ACTIVITE_LIST()
     FAL1.by_id(8739927)
     print("LISTE 1:")
     FAL1.print()
-    FAL1.update(6,DESCRIPTION = "Description des tâches")
+    FAL1.update(6, DESCRIPTION="Description des tâches")
     FAL1.print()
